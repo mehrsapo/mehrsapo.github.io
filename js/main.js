89d87fa737e24555919dd70a5d3a25b450a9c0ae
mehrsapo <mehrsapo@gmail.com>
2024-07-17 14:08:23 +0200

    copy poldap

---
+++ js/main.js
@@ -0,0 +1,62 @@
+angular.module('acadApp', ['ngRoute', 'ui.bootstrap', 'ngResource'])
+
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl:'html/main.html'
+    })
+    .when('/pubs', {
+      controller:'PubsCtrl',
+      templateUrl:'html/pubs.html'
+    })
+    .when('/bio', {
+      templateUrl:'html/bio.html'
+    })
+    .when('/more', {
+      templateUrl:'html/more.html'
+    })
+    .when('/teaching', {
+        controller: 'TeachCtrl',
+      templateUrl:'html/teaching.html'
+    })
+    .when('/talks', {
+      templateUrl:'html/talks.html'
+    })
+    .otherwise({
+      redirectTo:'html/main.html'
+    })
+
+  $locationProvider.html5Mode(false);
+  $locationProvider.hashPrefix('');
+
+})
+.controller('mainCtrl', ['$scope', '$location', '$uibModal', function($scope, $location, $uibModal) {
+  var updateButton = function() {
+    $scope.button = $location.url();
+  };
+
+  $scope.navbarCollapsed = true;
+
+  $scope.$on('$routeChangeStart', updateButton);
+
+  $scope.open_pic = function(pic_location) {
+    var modalInstance = $uibModal.open({
+      animation: false,
+      templateUrl: 'html/picmodal.html',
+      controller: 'PicModalCtrl',
+      size: 'lg',
+      resolve: {
+        pic_location: function () {
+          return pic_location
+        }
+      },
+      windowClass: 'pic-modal',
+      backdropClass: 'pic-modal-back'
+    }).result.then(function(){}, function(res){});
+  }
+}])
+.filter('floor', function() {
+  return function(input) {
+    return Math.floor(input);
+  };
+});
